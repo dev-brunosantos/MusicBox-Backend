41331a4293f0dc84218d9b8966194b826b3faf7c
Bruno Santos <brunosdesenvolvedor@gmail.com>
2026-03-02 22:49:07 -0300

    feat: criação dos primeiros modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DbModule } from './db/db.module';
+import { UsuarioModule } from './modules/usuario/usuario.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { AlunoModule } from './modules/aluno/aluno.module';
+import { TurmasModule } from './modules/turmas/turmas.module';
+import { PagamentoModule } from './modules/pagamento/pagamento.module';
 
 @Module({
-  imports: [DbModule],
+  imports: [DbModule, UsuarioModule, AuthModule, AlunoModule, TurmasModule, PagamentoModule],
   controllers: [AppController],
   providers: [AppService],
 })
